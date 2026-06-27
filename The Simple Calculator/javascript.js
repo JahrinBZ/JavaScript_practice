2083ae8a5f4e74a8e8d08835b6b15e397e81be37
Jahrin Binte Zahid <155414933+JahrinBZ@users.noreply.github.com>
2026-06-27 20:30:28 +0600

    Simple Calculator

---
+++ The Simple Calculator/javascript.js
@@ -0,0 +1,13 @@
+let firstNum = prompt("Enter the 1st number :");
+let secondNum = prompt("Enter the 2nd number :");
+let operator = prompt("The operator will be : ");
+
+if (operator == '+')
+    console.log(firstNum + secondNum);
+else if(operator == '-')
+    console.log(firstNum - secondNum);
+else if (operator == '*')
+    console.log(firstNum * secondNum);
+else if (operator == '/')
+    console.log(firstNum / secondNum);
+
