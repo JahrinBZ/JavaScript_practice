44113d3577354ebe34fa37d1d3c8ef740cf4b555
Jahrin Binte Zahid <155414933+JahrinBZ@users.noreply.github.com>
2026-06-27 21:26:56 +0600

    FizzBuzz

---
+++ The FizzBuzz Challenge/javascript.js
@@ -0,0 +1,7 @@
+let num = prompt("Enter a number: ");
+if (num % 3 == 0 && num % 5 != 0)
+    console.log("Fizz");
+else if (num % 5 == 0 && num % 3 != 0)
+    console.log("Buzz");
+else if (num % 3 ==0 && num % 5 == 0)
+    console.log("FizzBuzz");
